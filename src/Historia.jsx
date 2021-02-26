a087f561d0a372ab3a23751face3fddda75a1ccf
Leonardo Rodrigues <leonardo.rodriges@tegis.com.br>
2021-02-26 17:16:15 -0300

    Adiciona página história

--- src/Historia.jsx
+++ src/Historia.jsx
@@ -1,10 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./css/Historia.css";
 
 function Historia() {
+  const target = document.getElementsByClassName("anime");
+  const animationClass = "anime-start";
+  const offset = (window.innerHeight * 3) / 4;
+
+  useEffect(() => {
+    handleScroll();
+    window.onscroll = () => debounce(handleScroll(), 200);
+    function handleScroll() {
+      var documentTop = window.pageYOffset;
+      for (var i = 0; i < target.length; i++) {
+        var itemTop = target[i].offsetTop;
+        if (documentTop > itemTop - offset) {
+          target[i].className = "anime " + animationClass;
+        } else {
+          target[i].className = target[i].className.replace(animationClass, "");
+        }
+      }
+    }
+  }, [offset, target]);
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
   return (
-
-      <div>Ola</div>
-
+    <div id="body-historia">
+      <h1>About us</h1>
+      <section className="anime">
+        <div className="grid-text">
+          <h2>Obejetivo da pag</h2>
+          <p>
+            Essa pag web foi desenvolvida por motivos de aprendizagem,nela
+            aprendemos e usamos, HTML,CSS,JS,JQUERY,PHP e o framework
+            Materialize.
+          </p>
+        </div>
+        <div className="grid-img">
+          <img src="img/img2.png" alt="Img 2" />
+        </div>
+      </section>
+      <section className="anime">
+        <div className="grid-img img-right">
+          <img src="img/img4.png" alt="Img 4" />
+        </div>
+        <div className="grid-text">
+          <h2>Sobre o que é essa página?</h2>
+          <p>
+            É uma página de mídia de aúdio(Música),onde você pode curtir sua
+            playlist.
+          </p>
+        </div>
+      </section>
+      <section className="anime">
+        <div className="grid-text">
+          <h2>Utilidade</h2>
+          <p>
+            Essa página web tem a utilidade de trazer a aprendizagem das
+            linguagens citadas a cima, frameworks, estilização e como preencher
+            conteúdo, de uma maneira responsivo para que fique adequado o acesso
+            em computador e celulares. Também servindo como base para orientar
+            em alguns processos de programação.
+          </p>
+        </div>
+        <div className="grid-img">
+          <img src="img/img1.png" alt="Img 1" />
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/Template.jsx
+++ src/Template.jsx
@@ -1,133 +1,128 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Template = ({children}) => {
+const Template = ({ children }) => {
+  var now = new Date();
 
-    
-   var now = new Date();
-
-
-    return (
-      <>
-        <div className="navbar-fixed" style={{ zIndex: 998 }}>
-          <nav className=" purple darken-2">
-            <ul
-              id="slide-out"
-              className="sidenav"
-              style={{
-                backgroundImage: "https://wallpapercave.com/wp/wp4892382.jpg",
-              }}
-            >
-              <li>
-                <div className="user-view">
-                  <div className="background">
-                    <img id="img" src="img/fundoside.gif" alt="Fundo" />
-                  </div>
-                  <a href="#">
-                    <i className="fab fa-angrycreative fa-4x"></i>
-                  </a>
+  return (
+    <>
+      <div className="navbar-fixed" style={{ zIndex: 998 }}>
+        <nav className=" purple darken-2">
+          <ul
+            id="slide-out"
+            className="sidenav"
+            style={{
+              backgroundImage: "https://wallpapercave.com/wp/wp4892382.jpg",
+            }}
+          >
+            <li>
+              <div className="user-view">
+                <div className="background">
+                  <img id="img" src="img/fundoside.gif" alt="Fundo" />
                 </div>
+                <a href="#">
+                  <i className="fab fa-angrycreative fa-4x"></i>
+                </a>
+              </div>
+            </li>
+            <li>
+              <a style={{ color: "violet" }} href="/">
+                Home
+              </a>
+            </li>
+            <li>
+              <a style={{ color: "violet" }} href="/Historia">
+                História
+              </a>
+            </li>
+            <li>
+              <a style={{ color: "violet" }} href="/Radio">
+                Rádio
+              </a>
+            </li>
+            <li>
+              <a style={{ color: "violet" }} href="/Contato">
+                Contato
+              </a>
+            </li>
+          </ul>
+          <a href="#" data-target="slide-out" className="sidenav-trigger">
+            <i className="fas fa-align-justify"></i>
+          </a>
+          <div className="nav-wrapper">
+            <Link className="brand-logo" to="/">
+              <i className="fab fa-angrycreative fa-3x"></i>
+            </Link>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <a style={{ color: "violet" }} href="/">
-                  Home
-                </a>
+                <Link to="/historia">História</Link>
               </li>
               <li>
-                <a style={{ color: "violet" }} href="/Historia">
-                  História
-                </a>
+                <Link to="/radio">Rádio</Link>
               </li>
               <li>
-                <a style={{ color: "violet" }} href="/Radio">
-                  Rádio
-                </a>
-              </li>
-              <li>
-                <a style={{ color: "violet" }} href="/Contato">
-                  Contato
-                </a>
+                <Link to="/contato">Contato</Link>
               </li>
             </ul>
-            <a href="#" data-target="slide-out" className="sidenav-trigger">
-              <i className="fas fa-align-justify"></i>
-            </a>
-            <div className="nav-wrapper">
-              <a href="/" className="brand-logo">
-                {" "}
-                <i className="fab fa-angrycreative fa-3x"></i>
-              </a>
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
+          </div>
+        </nav>
+      </div>
+
+      {children}
+
+      <footer id="footer" className="page-footer purple darken-2">
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text">
+                Para mais informações entre em contato com nós.
+              </h5>
+              <p className="grey-text text-lighten-4">
+                Você pode se comunicar com nossa equipe acessando a aba
+                contatos.
+              </p>
+            </div>
+            <div className="col l4 offset-l2 s12">
+              <h5 className="white-text">Mídias sociais</h5>
+              <ul>
                 <li>
-                  <a href="/">Home</a>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Facebook
+                  </a>
                 </li>
                 <li>
-                  <a href="/Historia">História</a>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Twitter
+                  </a>
                 </li>
                 <li>
-                  <a href="/Radio">Rádio</a>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Instagram
+                  </a>
                 </li>
                 <li>
-                  <a href="/Contato">Contato</a>
+                  <a className="grey-text text-lighten-3" href="#!">
+                    Whatsapp
+                  </a>
                 </li>
               </ul>
             </div>
-          </nav>
+          </div>
         </div>
-
-        {children}
-
-        <footer id="footer" className="page-footer purple darken-2">
+        <div className="footer-copyright">
           <div className="container">
-            <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">
-                  Para mais informações entre em contato com nós.
-                </h5>
-                <p className="grey-text text-lighten-4">
-                  Você pode se comunicar com nossa equipe acessando a aba
-                  contatos.
-                </p>
-              </div>
-              <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Mídias sociais</h5>
-                <ul>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Facebook
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Twitter
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Instagram
-                    </a>
-                  </li>
-                  <li>
-                    <a className="grey-text text-lighten-3" href="#!">
-                      Whatsapp
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            {now.getFullYear()} © Matheus, Leonardo e João
+            <a className="grey-text text-lighten-4 right" href="#!">
+              Site sem fins lucrativos e roubo de direitos autorais.
+            </a>
           </div>
-          <div className="footer-copyright">
-            <div className="container">
-              { now.getFullYear() } © Matheus, Leonardo e João
-              <a className="grey-text text-lighten-4 right" href="#!">
-                Site sem fins lucrativos e roubo de direitos autorais.
-              </a>
-            </div>
-          </div>
-        </footer>
-      </>
-    );
-
-    
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Template;
